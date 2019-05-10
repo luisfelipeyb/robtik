ba9b6269001b25234bfe6233311d0b2964217dd5
luisfelipeyepezbarrios <luisfelipeyb@icloud.com>
2019-05-10 13:05:03 -0400

    2 nd Commit:
    Forms and Validations

--- app.py
+++ app.py
@@ -1,12 +1,45 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, flash, redirect, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.secret_key = os.urandom(64)
+app.config.update(
+    ADMIN_USER=os.environ.get('ADMIN_USER'),
+    ADMIN_PASSWORD=os.environ.get('ADMIN_PASSWORD')
+)
 
 
 @app.route('/')
 @app.route('/home')
-def hello_world():
+def home():
     return render_template('home.html')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('account'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('account'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+@app.route("/account", methods=['GET', 'POST'])
+def account():
+    render_template('account.html', title='Account')
 
 
 if __name__ == '__main__':
